3c4218b6ac5f487d69e406a2af6b1c1173ea06cd
Indah Mutiara <indah.mutiaralarasati@gmail.com>
2024-06-20 20:15:11 +0700

    third-push

--- js/script.js
+++ js/script.js
@@ -9,42 +9,46 @@ const formulaText = document.getElementById("formulaText");
 const celsiusLabel = document.getElementById("celsiusLabel");
 const fahrenheitLabel = document.getElementById("fahrenheitLabel");
 
-let isCelsiusToFahrenheit = true; 
+let isCelsiusToFahrenheit = true;
 
 function convertTemperature() {
-    let value;
-    let formula;
-    if (isCelsiusToFahrenheit) {
-      value = parseFloat(celsiusInput.value);
-      formula = "°F = (°C × 9/5) + 32";
-      fahrenheitInput.value = (value * 9/5) + 32;
-      conversionInfo.textContent = "Konversi dari Celsius (°C) ke Fahrenheit (°F)";
-  
-      calculationText.value = `Suhu ${value}°C sama dengan ${fahrenheitInput.value}°F \n\nRumus: ${formula} \n\nPenjelasan:
+  const value = parseFloat(celsiusInput.value);
+
+  // Check for empty input before proceeding
+  if (isNaN(value)) {
+    alert("Masukan nilai dahulu!");
+    return; // Exit the function if no input is provided
+  }
+
+  let formula;
+  if (isCelsiusToFahrenheit) {
+    formula = "°F = (°C × 9/5) + 32";
+    fahrenheitInput.value = (value * 9/5) + 32;
+    conversionInfo.textContent = "Konversi dari Celsius (°C) ke Fahrenheit (°F)";
+
+    calculationText.value = `Suhu ${value}°C sama dengan ${fahrenheitInput.value}°F \n\nRumus: ${formula} \n\nPenjelasan:
   
     ->${value}°C × 9/5 = ${(value * 9/5).toFixed(2)}°F
   
     ->${(value * 9/5).toFixed(2)}°F + 32 = ${fahrenheitInput.value}°F
   
-    
       `;
-    } else {
-      value = parseFloat(fahrenheitInput.value);
-      formula = "°C = (°F - 32) × 5/9";
-      celsiusInput.value = (value - 32) * 5/9;
-      conversionInfo.textContent = "Konversi dari Fahrenheit (°F) ke Celsius (°C)";
-  
-      calculationText.value = `Suhu ${value}°F sama dengan ${celsiusInput.value}°C \n\nRumus: ${formula} \n\nPenjelasan:
+  } else {
+    formula = "°C = (°F - 32) × 5/9";
+    celsiusInput.value = (value - 32) * 5/9;
+    conversionInfo.textContent = "Konversi dari Fahrenheit (°F) ke Celsius (°C)";
+
+    calculationText.value = `Suhu ${value}°F sama dengan ${celsiusInput.value}°C \n\nRumus: ${formula} \n\nPenjelasan:
   
     ->${value}°F - 32 = ${(value - 32).toFixed(2)}°F
   
     ->${(value - 32).toFixed(2)}°F × 5/9 = ${celsiusInput.value}°C
   
       `;
-    }
-    formulaText.textContent = formula;
   }
-      
+  formulaText.textContent = formula;
+}
+
 function resetInputs() {
   celsiusInput.value = "";
   fahrenheitInput.value = "";
